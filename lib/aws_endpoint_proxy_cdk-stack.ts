50497445c957164968f41c0e619ceeb5bcfda3af
lePaulo <lePaulo@users.noreply.github.com>
2019-11-07 10:44:50 +0100

    Update aws_endpoint_proxy_cdk-stack.ts

--- lib/aws_endpoint_proxy_cdk-stack.ts
+++ lib/aws_endpoint_proxy_cdk-stack.ts
@@ -5,94 +5,53 @@ import ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
 import path = require('path');
 import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
 import elb = require('@aws-cdk/aws-elasticloadbalancingv2');
+import { AwsCustomResource } from '@aws-cdk/custom-resources';
 
 export class AwsEndpointProxyCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const vpc = new ec2.Vpc(this, "SquidVPC", {
-      cidr: "10.0.0.0/26", // CIDR of the desired VPC (/28 is sufficient here)
-      maxAzs: 3 // Default is all AZs in region
-    });
-
-    const cluster = new ecs.Cluster(this, "AWSEndpointProxyCluster", {
-      vpc: vpc
-    });
-    
-    //Build the image from the right directory
-    const __dirname = path.resolve();
-    const dockerImageAsset = new DockerImageAsset(this, "AWSEndpointProxyDockerImageAsset", {
-      directory: path.join(__dirname, 'docker-image'),
-    });
-    
-    const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, "AWSEndpointProxyFargateTaskDefinition", {
-            memoryLimitMiB: 512,
-            cpu: 256
-    });
-    
-    const appContainer = fargateTaskDefinition.addContainer("Container", {
-            image: ecs.ContainerImage.fromRegistry(dockerImageAsset.imageUri)
-    });
-    
-    appContainer.addPortMappings({
-            containerPort: 3128
-        });
         
-    const executionRole = fargateTaskDefinition.obtainExecutionRole()
-    dockerImageAsset.repository.grantPull(executionRole.grantPrincipal)
+    // Build a simple VPC here, for instance with a CIDR of 10.0.0.0/24
     
-    const loadBalancer = new elb.NetworkLoadBalancer(this, "AWSEndpointProxyNetworkLoadBalancer", {
-      vpc: vpc,
-      crossZoneEnabled: true,
-      internetFacing: true // Default is false
-    })
     
-    const securityGroup = new ec2.SecurityGroup(this, "TaskSecurityGroup", {
-      vpc: vpc,
-    })
-    securityGroup.addIngressRule(ec2.Peer.ipv4("192.168.0.0/16"), ec2.Port.tcp(3128))
-    securityGroup.addIngressRule(ec2.Peer.ipv4("172.16.0.0/12"), ec2.Port.tcp(3128))
-    securityGroup.addIngressRule(ec2.Peer.ipv4("10.0.0.0/8"), ec2.Port.tcp(3128))
+    // Build a SecurityGroup for your Fargate task. 
+    // We will deal with a Squid process here, so please choose to open port 3128 
+    // on all private CIDR ranges (192.168.0.0/16, 172.16.0.0/12, 10.0.0.0/8).
     
-    const fargateService = new ecs.FargateService(this, "AWSEndpointProxyFargateService", {
-      cluster: cluster,
-      desiredCount: 2,
-      taskDefinition: fargateTaskDefinition,
-      securityGroup: securityGroup
-    });
     
-    const targetGroupSquid = new elb.NetworkTargetGroup(this, "NetNetworkTargetGroupSquid", {
-      port: 3128,
-      vpc: vpc,
-      targetType: elb.TargetType.IP
-    });
+    // Build your Docker image, using the Dockerfile you will have in the 'docker-image' 
+    // directory.
+    // (check https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-ecr-assets.DockerImageAsset.html).
     
-    const networkListener80 = new elb.NetworkListener(this, "NetworkListenerHTTP", {
-      loadBalancer: loadBalancer,
-      port: 80
-    });
     
-    networkListener80.addTargetGroups("DefaultTargetGroup", targetGroupSquid)
+    // Prepare a FargateTaskDefinition and add a container with the image created
+    // with the DockerImageAsset component and port 3128
     
-    const networkListener443 = new elb.NetworkListener(this, "NetworkListenerHTTPS", {
-      loadBalancer: loadBalancer,
-      port: 443
-    });
     
-    networkListener443.addTargetGroups("DefaultTargetGroup", targetGroupSquid)
+    // Grant the execution role of the FargateTaskDefinition with the ability to pull
+    // the docker image from the ECR registry of the DockerImageAsset component
+     
     
-    fargateService.attachToNetworkTargetGroup(targetGroupSquid)
+    // Create a NetworkLoadBalancer and attach to it a NetworkTargetGroup on port 3128
+    // with 2 NetworkListeners: one that will listen to port 80 and the other one to port 443.
     
-    const scaling = fargateService.autoScaleTaskCount({
-      minCapacity: 2,
-      maxCapacity: 10 
-    });
     
-    scaling.scaleOnCpuUtilization('CpuScaling', {
-      targetUtilizationPercent: 50,
-      scaleInCooldown: cdk.Duration.seconds(10),
-      scaleOutCooldown: cdk.Duration.seconds(10)
-    });
+    // Build an ECS Cluster
+    
+    
+    // Create a FargateService that will host our proxy layer application
+    
+    
+    // Add the network target group you have created for your load balancer to your
+    // FargateService component
+    
+    
+    // Add Fargate service autoscaling capabilities
+    
+    
+    // Bonus: Try implementing here an AwsCustomResource to create VPC Endpoint Service
+    // configuration on your Network Load Balancer, or you can also do it with the CLI.
+
     
   }
 }
